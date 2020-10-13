30bfd8898aedf2568fe982485e5d51e4aee62f07
lajtosdavid11 <lajtos.david11@gmail.com>
2020-10-13 08:11:10 +0200

    Add project files.

---
+++ AtletikaVerseny/Atleta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtletikaVerseny
+{
+    class Atleta
+    {
+        public string Rajtszam { get; private set; }
+        public string VezNev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesulet { get; private set; }
+        public int Ugras { get; private set; }
+    }
+}
